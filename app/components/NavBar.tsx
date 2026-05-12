9134072c1b4d2efa50d7cc30cebf32fead49affa
jorgemancipe <jorge_mancipe@yahoo.com>
2026-05-11 22:40:39 -0700

    Fix NavBar JSX cousing duplicated text

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,13 +2,19 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav class <div className="max-w-6xl mx-auto px-6 py-4 flex items-center gap-6">    <nav className="w-full bg-white border-b border-gray-200">
-        <Link href="/" className="font-semibold">
+    <nav className="w-full bg-white border-b border-gray-200">
+      <div className="max-w-6xl mx-auto px-6 py-4 flex gap-6 items-center">
+        <Link href="/" className="font-semibold hover:underline">
           Trading Journal
         </Link>
 
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/trades">Trades</Link>
+        <Link href="/dashboard" className="hover:underline">
+          Dashboard
+        </Link>
+
+        <Link href="/trades" className="hover:underline">
+          Trades
+        </Link>
       </div>
     </nav>
   );
