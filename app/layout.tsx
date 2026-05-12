d9f7aea03a60ff64781af15e1c07f6f4c79849c0
jorgemancipe <jorge_mancipe@yahoo.com>
2026-05-11 23:12:31 -0700

    Remove NavBar from RootLayout component
    Removed NavBar component from RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import NavBar from "./components/NavBar";
 
 export default function RootLayout({
   children,
@@ -9,7 +8,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen bg-gray-50 text-gray-900">
-        <NavBar />
         {children}
       </body>
     </html>
