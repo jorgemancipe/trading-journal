ef135b10f8a26cead54dbc3ddea49fb38c54a38f
jorgemancipe <jorge_mancipe@yahoo.com>
2026-05-12 22:30:31 -0700

    Wrap DashboardLayout children with TradesProvider

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,1 +1,12 @@
+"use client";
 
+import { TradesProvider } from "../context/TradesContext";
+export const dynamic = "force-dynamic";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <TradesProvider>{children}</TradesProvider>;
+}
